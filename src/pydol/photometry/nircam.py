7884ee7925e76c2ea6cd802c5a56d0a82544a41d
JACK3690 <avinashck90@gmail.com>
2024-08-29 15:41:57 -0600

    Added DOLPHOT completness for NIRCAM

--- src/pydol/photometry/nircam.py
+++ src/pydol/photometry/nircam.py
@@ -7,17 +7,20 @@ import numpy as np
 import multiprocessing as mp
 from pathlib import Path
 import subprocess
+import pandas as pd
+from ..scripts.catalog_filer import box
 
 param_dir_default = str(Path(__file__).parent.joinpath('params'))
 script_dir = str(Path(__file__).parent.joinpath('scripts'))
 
-def nircam_phot(cal_files, filter='f200w',output_dir='.', drz_path='.',
-                cat_name='', param_file=None):
+def nircam_phot(crf_files, filter='f200w',output_dir='.', drz_path='.',
+                cat_name='', param_file=None,sharp_cut=0.01,
+                crowd_cut=0.5):
     """
         Parameters
         ---------
-        cal_files: list,
-                    list of paths to JWST NIRCAM level 2 _cal.fits files
+        crf_files: list,
+                    list of paths to JWST NIRCAM level 3 _crf.fits files
         filter: str,
                 name of the NIRCAM filter being processed
         output_dir: str,
@@ -33,8 +36,8 @@ def nircam_phot(cal_files, filter='f200w',output_dir='.', drz_path='.',
         ------
         None
     """
-    if len(cal_files)<1:
-        raise Exception("cal_files cannot be EMPTY")
+    if len(crf_files)<1:
+        raise Exception("crf_files cannot be EMPTY")
 
     subprocess.run([f"nircammask {drz_path}.fits"], shell=True)
     if not os.path.exists(output_dir):
@@ -48,10 +51,10 @@ def nircam_phot(cal_files, filter='f200w',output_dir='.', drz_path='.',
       edit_params = False
 
     out_id = filter + cat_name
-    
+
     # Generating directories
     exps = []
-    for i,f in enumerate(cal_files):
+    for i,f in enumerate(crf_files):
         out_dir = f.split('/')[-1].split('.')[0]
 
         if not os.path.exists(f'{output_dir}/{out_dir}'):
@@ -112,16 +115,120 @@ def nircam_phot(cal_files, filter='f200w',output_dir='.', drz_path='.',
         phot_table['dec'] = coords[:,1]
 
         # Filtering stellar photometry catalog using Warfield et.al (2023)
-        phot_table1 = phot_table[ (phot_table['obj_sharpness']**2   <= 0.01) &
-                                    (phot_table['obj_crowd']    <=  0.5) &
-                                    (phot_table['type']         <=    2) &
-                                    (phot_table['obj_SNR']      >=    5)]
+        phot_table1 = phot_table[ (phot_table['obj_sharpness']**2<= sharp_cut) &
+                                    (phot_table['obj_crowd']<= crowd_cut) &
+                                    (phot_table['type'] <= 2)]
         flag_keys = []
         for key in phot_table1.keys():
-          if 'flag' in key:
-            flag_keys.append(key)
+            if 'flag' in key:
+                flag_keys.append(key)
         for i in flag_keys:
-          phot_table1  = phot_table1[phot_table1[i]<=2]
+            phot_table1  = phot_table1[phot_table1[i]<=2]
+
+        SNR_keys = []
+        for key in phot_table1.keys():
+            if 'SNR' in key:
+                SNR_keys.append(key)
+        for i in SNR_keys:
+            phot_table1  = phot_table1[phot_table1[i]>=5]
+
         phot_table.write(f'{output_dir}/{out_id}_photometry.fits', overwrite=True)
         phot_table1.write(f'{output_dir}/{out_id}_photometry_filt.fits', overwrite=True)
     print('NIRCAM Stellar Photometry Completed!')
+
+def nircam_phot_comp(crf_files,m=20, filter='f200w',output_dir='.', drz_path='.',
+                cat_name='', param_file=None,sharp_cut=0.01, crowd_cut=0.5,
+                ra=0,dec=0,width=24/3600,height=24/3600,ang=245):
+    """
+        Parameters
+        ---------
+        crf_files: list,
+                    list of paths to JWST NIRCAM level 2 _crf.fits files
+        filter: str,
+                name of the NIRCAM filter being processed
+        output_dir: str,
+                    path to output directory.
+                    Recommended: /photometry/
+        drz_path: str,
+                  path to level 3 drizzled image (_i2d.fits) image.
+                  It is recommended to be inside /photometry/
+        cat_name: str,
+                  Output photometry catalogs will have prefix filter + cat_name
+
+        Return
+        ------
+        None
+    """
+    if len(crf_files)<1:
+        raise Exception("crf_files cannot be EMPTY")
+
+    subprocess.run([f"nircammask {drz_path}.fits"], shell=True)
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    if param_file is None or not os.path.exists(param_file) :
+      raise Exception("param_file cannot be EMPTY")
+
+    out_id = filter + cat_name
+
+     # Completeness
+    tab = Table.read(drz_path)
+    tab_n = box(tab,ra, dec, width, height, angle=ang)
+    x = tab_n['x']
+    y = tab_n['y']
+    xx, yy = np.meshgrid(np.linspace(x.min()+10,x.max()-10,100),
+                         np.linspace(y.min()+10,y.max()-10,100))
+    x,y = xx.ravel().astype(int), yy.ravel().astype(int)
+    mag = m + x*0
+    ext = 1 + x*0
+    chip = 1 + x*0
+
+    df = pd.DataFrame(zip(ext,chip,x,y,mag),
+                      columns=['ext', 'chip', 'x', 'y', 'mag'])
+    df.to_csv(f'{output_dir}_fake_{m}_{out_id}.txt',sep=' ',
+              index=None,header=None)
+
+    with open([param_file]) as f:
+      dats = f.readlines()
+
+    for n,dat in enumerate(dats):
+      if 'FakeStars' in dat:
+        ind = n
+
+    dats[n] = f'FakeStars =   {output_dir}_fake_{m}_{out_id}.txt\n'
+    dat[n+1] = f'FakeOut =    {output_dir}_fake_{m}_{out_id}.fake\n'
+    if os.path.exists(f"{output_dir}/{out_id}_photometry.fits"):
+        # Running DOLPHOT NIRCAM
+        p = subprocess.Popen(["dolphot", f"{output_dir}/out", f"-p{param_file}"]
+                            , stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                             text=True)
+        while (line := p.stdout.readline()) != "":
+          print(line)
+    # Generating Astropy FITS Table
+
+        cmd = f"python {script_dir}/to_table_fake.py --o fake_out_{m}_{out_id}"
+        cmd += " --f {output_dir}/o{output_dir}_fake_{m}_{out_id}.fakeut"
+        out = subprocess.run([cmd], shell=True)
+
+        phot_table = Table.read(f"{output_dir}/fake_out_{m}_{out_id}.fits")
+
+        # Filtering stellar photometry catalog using Warfield et.al (2023)
+        phot_table1 = phot_table[ (phot_table['obj_sharpness']**2<= sharp_cut) &
+                                    (phot_table['obj_crowd']<= crowd_cut) &
+                                    (phot_table['type'] <= 2)]
+        flag_keys = []
+        for key in phot_table1.keys():
+            if 'flag' in key:
+                flag_keys.append(key)
+        for i in flag_keys:
+            phot_table1  = phot_table1[phot_table1[i]<=2]
+
+        SNR_keys = []
+        for key in phot_table1.keys():
+            if 'SNR' in key:
+                SNR_keys.append(key)
+        for i in SNR_keys:
+            phot_table1  = phot_table1[phot_table1[i]>=5]
+
+        phot_table1.write(f'{output_dir}/{out_id}_photometry_filt.fits', overwrite=True)
+    print('NIRCAM Completeness Completed!')
